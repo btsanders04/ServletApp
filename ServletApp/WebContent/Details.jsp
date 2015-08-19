1cf6fe3ab769a315dc61c6e3b858c9144b90c0cd
Brandon Sanders <btsanders04@gmail.com>
2015-08-19 14:50:53 -0400

    Customer Details Link Started

--- ServletApp/WebContent/Details.jsp
+++ ServletApp/WebContent/Details.jsp
@@ -1,9 +1,10 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
-	pageEncoding="ISO-8859-1"%>
+    pageEncoding="ISO-8859-1"%>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Details</title>
 <link rel="stylesheet" type="text/css"
 	href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
 <link rel="stylesheet" type="text/css" href="custom.css">
@@ -12,24 +13,13 @@
 <script
 	src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 
-<title>Customers</title>
 </head>
 <body>
-
-	<div class="container">
-		<h2>Customers</h2>
-		<p>List of Customers from Demo Customers</p>
-		<table
-			class="table table-striped table-bordered table-hover table-condensed\">
-			<thead>
-				<tr>
-					<th>First Name</th>
-					<th>Last Name</th>
-				</tr>
-			</thead>
-			${message}
-			</tbody>
-		</table>
-	</div>
+<div class="container">
+  <div class="jumbotron" align="center">
+    <h1>Details</h1> 
+    
+  </div>
+</div>
 </body>
 </html>
--- ServletApp/src/HelloWorld.java
+++ ServletApp/src/HelloWorld.java
@@ -1,5 +1,11 @@
 // Import required java libraries
 import java.io.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Properties;
 
 import javax.servlet.*;
 import javax.servlet.annotation.WebServlet;
@@ -9,13 +15,12 @@ import javax.servlet.http.*;
 @WebServlet("/HelloWorld")
 public class HelloWorld extends HttpServlet {
  
-  private String message;
+  private String message="";
 
 
   public void init() throws ServletException
   {
-      // Do required initialization
-      message = "Hello World";
+      message="";
   }
 
   public void doGet(HttpServletRequest request,
@@ -28,12 +33,47 @@ public class HelloWorld extends HttpServlet {
       // Actual logic goes here.
 //      PrintWriter out = response.getWriter();
     //  out.println("<h1>" + message + "</h1>"); 
+      try {
+    	  message="";
+      String url = "jdbc:oracle:thin:testuser/password@localhost"; 
+      Class.forName("oracle.jdbc.driver.OracleDriver");
+      //properties for creating connection to Oracle database
+      Properties props = new Properties();
+      props.setProperty("user", "testdb");
+      props.setProperty("password", "password");
+    
+      //creating connection to Oracle database using JDBC
+      Connection conn;
+	
+		conn = DriverManager.getConnection(url,props);
+		String sql ="select * from demo_customers";
+		PreparedStatement preStatement = conn.prepareStatement(sql);
+		  
+	    ResultSet result = preStatement.executeQuery();
+	   // System.out.println(result);
+       
+	    
+	    while(result.next()){
+	      		 message +=  "<tr>" + "<th> <a href=\"details.jsp?customerId="+result.getInt("customer_id")+"\">"+result.getString("cust_first_name") + "</a></th>" +
+	      				"<th>" + result.getString("cust_last_name") + "</th>";
+	    }
+	} catch (SQLException | ClassNotFoundException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+
+      
+     
+      
+      
       request.setAttribute("message", message);
       getServletContext().getRequestDispatcher("/output.jsp").forward(request,response);
    } 
+  
+  
 
    public void destroy() 
    { 
-     // do nothing. 
+    //message=""; 
    } 
 }
